a260c5867f29ad4a10b80d24cda7ed09179b2ceb
Sean Mebust <smebust20@amherst.edu>
2018-12-05 17:49:42 -0500

    updating learners

--- learners.py
+++ learners.py
@@ -38,55 +38,100 @@ class ToyRegressor(Regressor):
     
 class OLS(Regressor):
     def __init__(self):
-        pass
+        self.theta = 0
 
     def fit(self, X, y):
-        pass
+        self.theta = np.linalg.solve(np.dot(np.transpose(X),X), np.dot(np.transpose(X), y))
 
     def predict(self, X):
-        pass
+        return np.dot(np.transpose(X), self.theta)
+        
 
     
 class RidgeRegression(Regressor):
     def __init__(self, lamb):
-        pass
+        self.theta = 0
+        self.weight = lamb
         
     def fit(self, X, y):
-        pass
+        I = np.identity(X.shape[0])
+        n = X.shape[0]
+        a = ((1/n)*np.dot(np.transpose(X), X)) + (self.weight*I)
+        b = (1/n)*np.dot(np.transpose(X), y)
+        self.theta = np.linalg.solve(a,b)
 
     def predict(self, X):
-        pass
+        return np.dot(np.transpose(X), self.theta)
 
 
 class GeneralizedRidgeRegression(Regressor):
     def __init__(self, reg_weights):
-        pass
+        self.theta = 0
+        self.lambs = reg_weights
         
     def fit(self, X, y):
-        pass
+        n = X.shape[0]
+        a = ((1/n)*np.dot(np.transpose(X), X)) + np.diag(self.lambs)
+        b = (1/n)*np.dot(np.transpose(X), y)
+        self.theta = np.linalg.solve(a,b)
 
     def predict(self, X):
-        pass
+        return np.dot(np.transpose(X), self.theta)
     
 
 class DualRidgeRegression(Regressor):
     def __init__(self, lamb, kernel):
-        pass
+        self.a = 0
+        self.weight = lamb
+        self.kernel = kernel
         
     def fit(self, X, y):
-        pass
+        I = np.identity(X.shape[0])
+        K = np.zeros((X.shape[0], X.shape[0]))
+        for i in range(K.shape[0]):
+            for j in range(K.shape[0]):
+                K[i,j] = self.kernel(X[i], X[j])
+        x = K + self.weight*I
+        self.a = np.linalg.solve(x,y)
+
 
     def predict(self, X):
-        pass
+        K = np.zeros((X.shape[0], X.shape[0]))
+        for i in range(K.shape[0]):
+            for j in range(K.shape[0]):
+                K[i,j] = self.kernel(X[i], X[j])
+        return np.dot(np.transpose(self.a), K)
 
     
 class AdaptiveLinearRegression(Regressor):
     def __init__(self, kernel): # note kernel used in totally different way
-        pass
+        self.kernel = kernel
+        self.X = 0
+        self.y = 0
 
     def fit(self, X, y):
-        pass
+        self.X = X
+        self.y = y
 
     def predict(self, X):
-        pass
+        toRet = np.zeros(X.shape[0])
+        for i in range(X.shape[0]):
+            K = np.zeros(self.y.shape[0])
+            for j in range(self.y.shape[0]):
+                K[j] = self.kernel(X[i], self.X[j]) # similarity measure between x and x(i)
+            K_mat = np.diag(K)
+
+            a = np.dot(np.transpose(X), K_mat)
+            b = np.dot(a, K_mat)
+            c = np.dot(b, self.X)
+            a1 = np.dot(np.transpose(X), K_mat)
+            b1 = np.dot(a1, self.y)
+ 
+            aToDot = np.linalg.solve(c, b1)
+            toRet[i] = np.dot(aToDot, self.X[i])
+
+        return toRet
+
+
+
     
